fbccbf5f3b47fea561df01ce66dee91ff1931470
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-02-11 14:45:07 -0800

    updated home button functionality

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 export default function SavedList(props) {
+  const history = useHistory();
+
+  const backHome = () => {
+    history.push('/');
+  }
+
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <Link to={'/'}><div className="home-button">Home</div></Link>
+      <button className="home-button" onClick={backHome}>Home</button>
     </div>
   );
 }
+
+
+// NOTE: adding a <Link></Link> tag to the home button div works as well, and that's initially how I set it
+// but I'm going to deviate and use the useHistory hook to practice.
